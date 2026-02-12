6c5eae78f76e2be30229f81fb0442ab94cec5841
PavelTarakanov <t.p.d.23102007@mail.ru>
2026-02-12 19:42:14 +0300

    very last commit

--- get-led/double-push.py
+++ get-led/double-push.py
@@ -10,6 +10,7 @@ leds = [16, 12, 25, 17, 27, 23, 22, 24]
 up = 9
 down = 10
 num = 0
+value = 0
 sleep_time = 0.2
 
 GPIO.setup(leds, GPIO.OUT)
@@ -37,4 +38,8 @@ while True:
             num = 255
         print(num, dec2bin(num))
         time.sleep(sleep_time)
+    value = num
+    for i in range(8):
+        GPIO.output()(leds[i], value%2)
+        value = value//2
 
